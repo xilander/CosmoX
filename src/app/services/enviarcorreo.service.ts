95af6d2810a9a4e3dcd8892867afe3c0156a76ec
Xilander <xilander@hotmail.com>
2021-07-14 23:34:59 -0500

    Modificación del formato de correo enviado

--- src/app/services/enviarcorreo.service.ts
+++ src/app/services/enviarcorreo.service.ts
@@ -1,9 +1,9 @@
 import { HttpClient } from '@angular/common/http';
-import { EventEmitter, Injectable, Input, Output } from '@angular/core';
+import { Injectable, Input } from '@angular/core';
 import { Form } from '@angular/forms';
 import { environment } from 'src/environments/environment';
 import Swal from 'sweetalert2';
-import { Subject } from "rxjs";
+import { from, Subject } from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -21,11 +21,22 @@ export class EnviarCorreoService {
 
   enviaContacto(form: any) {
 
+    var cuerpoMensaje = `<i>~~~ Correo enviado desde la página web de CosmoX: ~~~</i> <br/><br/>
+              Nombre : <b>${form.nombre}</b>  <br/>
+              Correo : <b>${form.correo}</b>  <br/>
+              Telefono: <b>${form.telefono}</b><br/>
+              Mensaje: <b>${form.mensaje} </b> <br/><br/>
+              <i>~~~ Fin del mensaje ~~~</i>`;
+
+    console.log(cuerpoMensaje);
+    
+
     let formData = new FormData();
     formData.append('ToEmail', 'admin@cosmox.mx');
     formData.append('Subject', 'Contacto desde la pagina web');
-    formData.append('Body', 'Nombre: ' + form.nombre + ', Telefono: ' + form.telefono + ',  Mensaje: ' + form.mensaje);
-
+    // formData.append('Body', 'Nombre: ' + form.nombre +  ', Correo: ' + form.correo +  ', Telefono: ' + form.telefono + ',  Mensaje: ' + form.mensaje);
+    formData.append('Body', cuerpoMensaje); 
+    
     fetch(this.apiURL, {
       method: 'POST',
       body: formData,
